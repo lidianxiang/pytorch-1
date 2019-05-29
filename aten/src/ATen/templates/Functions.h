mtli/pytorch

Replace Type dispatch with ATenDispatch
gh-metadata: pytorch pytorch 20471 gh/li-roy/17/head

aten/src/ATen/templates/Functions.h
@@ -15,6 +15,7 @@
 #include <ATen/core/Reduction.h>
 #include <c10/util/Optional.h>
 #include <ATen/TensorUtils.h>
+#include <ATen/core/ATenDispatch.h>
 
 namespace at {
 
@@ -70,14 +71,24 @@ inline Tensor from_blob(
 
 namespace detail {
 
-static inline TypeExtendedInterface & infer_type(const Tensor & t) {
+static inline Backend infer_backend(const Tensor & t) {
   AT_CHECK(t.defined(), "undefined Tensor");
-  return getType(t);
+  return tensorTypeIdToBackend(t.type_id());
 }
-static inline TypeExtendedInterface & infer_type(const TensorList & tl) {
+static inline Backend infer_backend(const TensorList & tl) {
   AT_CHECK(tl.size() > 0, "expected a non-empty list of Tensors");
-  return getType(tl[0]);
+  return tensorTypeIdToBackend(tl[0].type_id());
 }
+
+static inline bool infer_is_variable(const Tensor & t) {
+  AT_CHECK(t.defined(), "undefined Tensor");
+  return t.is_variable();
+}
+static inline bool infer_is_variable(const TensorList & tl) {
+  AT_CHECK(tl.size() > 0, "expected a non-empty list of Tensors");
+  return tl[0].is_variable();
+}
+
 
 } // namespace detail
 

torch/csrc/autograd/VariableTypeManual.cpp
@@ -223,7 +223,7 @@ void VariableType::set_data(Tensor & self, Tensor new_data) const {
 }
 
 // We don't have an outplace copy, so this can't be generated automatically
-Tensor & VariableType::copy_(Tensor & self, const Tensor & src, bool non_blocking) const {
+Tensor & VariableType::copy__127(Tensor & (*_op)(Tensor &, const Tensor &, bool), Tensor & self, const Tensor & src, bool non_blocking) {
   jit::Value* output = nullptr;
   if(torch::jit::tracer::isTracing()) {
     const jit::tracer::TracingState& state = *jit::tracer::getTracingState();
@@ -259,7 +259,7 @@ Tensor & VariableType::copy_(Tensor & self, const Tensor & src, bool non_blockin
   }
   {
     at::AutoNonVariableTypeMode non_var_type_mode(true);
-    baseType->copy_(self_, src_, non_blocking);
+    (*_op)(self_, src_, non_blocking);
   }
   increment_version(self);
   rebase_history(as_variable_ref( self ), std::move(grad_fn));
@@ -269,7 +269,7 @@ Tensor & VariableType::copy_(Tensor & self, const Tensor & src, bool non_blockin
   return self;
 }
 
-Tensor & VariableType::resize_(Tensor & self, IntArrayRef size) const {
+Tensor & VariableType::resize__191(Tensor & (*_op)(Tensor &, IntArrayRef), Tensor & self, IntArrayRef size) {
   auto& self_ = unpack(self, "self", 0);
   if (as_variable_ref(self).requires_grad()) {
     AT_ERROR("cannot resize variables that require grad");
@@ -281,12 +281,12 @@ Tensor & VariableType::resize_(Tensor & self, IntArrayRef size) const {
   }
   {
     at::AutoNonVariableTypeMode non_var_type_mode(true);
-    baseType->resize_(self_, size);
+    (*_op)(self_, size);
   }
   return self;
 }
 
-Tensor & VariableType::resize_as_(Tensor & self, const Tensor & the_template) const {
+Tensor & VariableType::resize_as__589(Tensor & (*_op)(Tensor &, const Tensor &), Tensor & self, const Tensor & the_template) {
   auto& self_ = unpack(self, "self", 0);
   auto& the_template_ = unpack(the_template, "the_template", 1);
   if (as_variable_ref(self).requires_grad()) {
@@ -298,12 +298,12 @@ Tensor & VariableType::resize_as_(Tensor & self, const Tensor & the_template) co
   }
   {
     at::AutoNonVariableTypeMode non_var_type_mode(true);
-    baseType->resize_as_(self_, the_template_);
+    (*_op)(self_, the_template_);
   }
   return self;
 }
 
-Tensor VariableType::detach(const Tensor & self) const {
+Tensor VariableType::detach_467(Tensor (*_op)(const Tensor &), const Tensor & self) {
   RECORD_FUNCTION("detach", std::vector<c10::IValue>({self}));
 
   torch::jit::Node* node = nullptr;
@@ -324,7 +324,7 @@ Tensor VariableType::detach(const Tensor & self) const {
   return std::move(result);
 }
 
-Tensor & VariableType::detach_(Tensor & self) const {
+Tensor & VariableType::detach__468(Tensor & (*_op)(Tensor &), Tensor & self) {
   RECORD_FUNCTION("detach_", std::vector<c10::IValue>({self}));
 
   torch::jit::Node* node = nullptr;

lidianxiang/pytorch-1

aten/src/ATen/templates/Functions.h
@@ -15,6 +15,7 @@
 #include <ATen/core/Reduction.h>
 #include <c10/util/Optional.h>
 #include <ATen/TensorUtils.h>
+#include <ATen/core/ATenDispatch.h>
 
 namespace at {
 
@@ -70,14 +71,24 @@ inline Tensor from_blob(
 
 namespace detail {
 
-static inline TypeExtendedInterface & infer_type(const Tensor & t) {
+static inline Backend infer_backend(const Tensor & t) {
   TORCH_CHECK(t.defined(), "undefined Tensor");
-  return getType(t);
+  return tensorTypeIdToBackend(t.type_id());
 }
-static inline TypeExtendedInterface & infer_type(const TensorList & tl) {
+static inline Backend infer_backend(const TensorList & tl) {
   TORCH_CHECK(tl.size() > 0, "expected a non-empty list of Tensors");
-  return getType(tl[0]);
+  return tensorTypeIdToBackend(tl[0].type_id());
 }
+
+static inline bool infer_is_variable(const Tensor & t) {
+  TORCH_CHECK(t.defined(), "undefined Tensor");
+  return t.is_variable();
+}
+static inline bool infer_is_variable(const TensorList & tl) {
+  TORCH_CHECK(tl.size() > 0, "expected a non-empty list of Tensors");
+  return tl[0].is_variable();
+}
+
 
 } // namespace detail
 

torch/csrc/autograd/VariableTypeManual.cpp
@@ -219,7 +219,7 @@ void VariableType::set_data(Tensor & self, Tensor new_data) const {
 }
 
 // We don't have an outplace copy, so this can't be generated automatically
-Tensor & VariableType::copy_(Tensor & self, const Tensor & src, bool non_blocking) const {
+Tensor & VariableType::copy__127(Tensor & (*_op)(Tensor &, const Tensor &, bool), Tensor & self, const Tensor & src, bool non_blocking) {
   jit::Value* output = nullptr;
   if(torch::jit::tracer::isTracing()) {
     const jit::tracer::TracingState& state = *jit::tracer::getTracingState();
@@ -255,7 +255,7 @@ Tensor & VariableType::copy_(Tensor & self, const Tensor & src, bool non_blockin
   }
   {
     at::AutoNonVariableTypeMode non_var_type_mode(true);
-    baseType->copy_(self_, src_, non_blocking);
+    (*_op)(self_, src_, non_blocking);
   }
   increment_version(self);
   rebase_history(as_variable_ref( self ), std::move(grad_fn));
@@ -265,7 +265,7 @@ Tensor & VariableType::copy_(Tensor & self, const Tensor & src, bool non_blockin
   return self;
 }
 
-Tensor & VariableType::resize_(Tensor & self, IntArrayRef size) const {
+Tensor & VariableType::resize__189(Tensor & (*_op)(Tensor &, IntArrayRef), Tensor & self, IntArrayRef size) {
   auto& self_ = unpack(self, "self", 0);
   if (as_variable_ref(self).requires_grad()) {
     AT_ERROR("cannot resize variables that require grad");
@@ -277,12 +277,12 @@ Tensor & VariableType::resize_(Tensor & self, IntArrayRef size) const {
   }
   {
     at::AutoNonVariableTypeMode non_var_type_mode(true);
-    baseType->resize_(self_, size);
+    (*_op)(self_, size);
   }
   return self;
 }
 
-Tensor & VariableType::resize_as_(Tensor & self, const Tensor & the_template) const {
+Tensor & VariableType::resize_as__591(Tensor & (*_op)(Tensor &, const Tensor &), Tensor & self, const Tensor & the_template) {
   auto& self_ = unpack(self, "self", 0);
   auto& the_template_ = unpack(the_template, "the_template", 1);
   if (as_variable_ref(self).requires_grad()) {
@@ -294,12 +294,12 @@ Tensor & VariableType::resize_as_(Tensor & self, const Tensor & the_template) co
   }
   {
     at::AutoNonVariableTypeMode non_var_type_mode(true);
-    baseType->resize_as_(self_, the_template_);
+    (*_op)(self_, the_template_);
   }
   return self;
 }
 
-Tensor VariableType::detach(const Tensor & self) const {
+Tensor VariableType::detach_465(Tensor (*_op)(const Tensor &), const Tensor & self) {
   RECORD_FUNCTION("detach", std::vector<c10::IValue>({self}));
 
   torch::jit::Node* node = nullptr;
@@ -320,7 +320,7 @@ Tensor VariableType::detach(const Tensor & self) const {
   return std::move(result);
 }
 
-Tensor & VariableType::detach_(Tensor & self) const {
+Tensor & VariableType::detach__466(Tensor & (*_op)(Tensor &), Tensor & self) {
   RECORD_FUNCTION("detach_", std::vector<c10::IValue>({self}));
 
   torch::jit::Node* node = nullptr;